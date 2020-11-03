771a77df78f1f6d080f072298ae9fa8cdd5c6558
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-11-03 16:26:54 +0100

    refactor text gen ds

---
+++ data/pre_proc/tmp_pad_nums_in_fj.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+import pickle as pkl
+from allennlp.modules.elmo import Elmo
+import fastText
+from tqdm import tqdm
+import yaml
+import ipdb
+from data.datasets import TextGenerationDataset
+
+
+def main(args):
+    global CFG
+    with open("config.yaml", "r") as ymlfile:
+        CFG = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+
+    with ipdb.launch_ipdb_on_exception():
+        index_file = os.path.join(CFG["gpudatadir"], args.voc_index + args.ft_type + ".pkl")
+        with open(index_file, 'rb') as f:
+            index = pkl.load(f)
+        print("index loaded.")
+
+        for split in ["TEST", "VALID", "TRAIN"]:
+            num_tokens = 0
+            num_pad = 0
+            input_file = os.path.join(CFG["gpudatadir"], args.base_file + split + ".json")
+            dataset = TextGenerationDataset(CFG["gpudatadir"], input_file, None, split, args.ft_type, args.max_seq_length, None, 0, True)
+            for tup in tqdm(dataset, desc="Parsing dataset for split " + split):
+                for tok in tup["first_job"]:
+                    if tok == index["PAD"]:
+                        num_pad += 1
+                    num_tokens += 1
+            print(str(num_pad*100/num_tokens) + "% of tokens are PAD tokens")
+            ipdb.set_trace()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base_file", type=str, default="bp_3jobs_desc_edu_skills_industry_date_company_FR")
+    parser.add_argument("--build_ind_dict", type=str, default="False")
+    parser.add_argument("--voc_index", type=str, default="index_40k_")
+    parser.add_argument("--ft_type", type=str, default="fs")
+    parser.add_argument("--max_seq_length", type=int, default=64)
+    parser.add_argument("--load_dataset", type=str, default="False")
+    args = parser.parse_args()
+    main(args)
