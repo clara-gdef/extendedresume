9ea7ff6ebc041b76ba98514fb7ec311cae973c62
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-11-03 15:43:24 +0100

    refactor text gen ds

--- models/train/first_job_generator.py
+++ models/train/first_job_generator.py
@@ -38,7 +38,7 @@ def main(hparams):
     dataset_train, dataset_valid = datasets[0], datasets[1]
 
     tmp = dataset_train.tuples
-    for i in range(5):
+    for i in range(3):
         dataset_train.tuples.extend(tmp)
 
     if hparams.ft_type != 'elmo':
