5a5773eee831d6d0664acde6c522d569587ef98a
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-11-09 14:06:02 +0100

    try a hard rollback to when ti worked

--- models/classes/DecoderLSTM.py
+++ models/classes/DecoderLSTM.py
@@ -1,6 +1,5 @@
 import torch
 import pytorch_lightning as pl
-import ipdb
 
 
 class DecoderLSTM(pl.LightningModule):
@@ -9,12 +8,12 @@ class DecoderLSTM(pl.LightningModule):
         self.lstm = torch.nn.LSTM(input_size + 1, hidden_size, 1,  batch_first=True)
         self.lin_out = torch.nn.Linear(hidden_size, out_size)
 
-    def forward(self, encoder_representation, token, previous_state):
-        enc_rep = encoder_representation.expand(token.shape[0], token.shape[-1], encoder_representation.shape[-1])
-        # enc_rep = encoder_representation
-        inputs = torch.cat([enc_rep.type(torch.float32), token.type(torch.float32).cuda().transpose(2, 1)], dim=2)
+    def forward(self, encoder_representation, token):
+        #enc_rep = encoder_representation.expand(token.shape[0], token.shape[1], encoder_representation.shape[-1]).transpose(1, 0)
+        enc_rep = encoder_representation
+        inputs = torch.cat([enc_rep.type(torch.float32).transpose(1, 0), token.type(torch.float32).unsqueeze(-1).transpose(1, 0).cuda()], dim=2)
 
-        out, hidden = self.lstm(inputs, previous_state)
+        out, hidden = self.lstm(inputs)
         results = self.lin_out(out)
 
-        return results, hidden
+        return results.transpose(1, 0), hidden
