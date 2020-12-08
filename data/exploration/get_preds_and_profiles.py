4c82a18f1e85cdcf69d41ea308fbe59cedf7ebac
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-12-08 11:01:30 +0100

    get preds

--- data/exploration/get_preds_and_profiles.py
+++ data/exploration/get_preds_and_profiles.py
@@ -3,11 +3,14 @@ import os
 import ipdb
 import argparse
 import pickle as pkl
+from tqdm import tqdm
 import torch
 from torch.utils.data import DataLoader
+import json
 import yaml
 from data.datasets import AggregatedEduDataset
 from models.classes.EvalModels import EvalModels
+from utils import get_ind_class_dict, word_seq_into_list
 from utils.model import collate_for_edu, get_model_params, get_latest_model
 
 
@@ -23,38 +26,95 @@ def init(hparams):
 
 
 def main(hparams):
-    xp_title = hparams.model_type + "_" + hparams.ft_type + str(hparams.b_size) + "_" + str(hparams.lr) + '_' + str(hparams.wd)
-    datasets = load_datasets(hparams, ["TEST"])
-    dataset_test = datasets[0]
+    predictions = get_predictions(hparams.load_dataset)
+    ind_classes = get_ind_class_dict(False, CFG)
+    rev_ind_class = {v:k for k, v in ind_classes.items()}
+    right_ind, wrong_ind = get_right_wrong_pred(predictions, "ind")
+    right_pred_file =  os.path.join(CFG["gpudatadir"],"ind_right_pred_edu.txt")
+    wrong_pred_file =  os.path.join(CFG["gpudatadir"],"ind_wrong_pred_edu.txt")
+    base_file = os.path.join(CFG["gpudatadir"], "bp_3jobs_desc_edu_skills_industry_date_company_FR_TEST.json")
+    with open(right_pred_file, 'a+') as rpf:
+        with open(wrong_pred_file, 'a+') as wpf:
+            with open(base_file, 'r') as f:
+                num_lines = sum(1 for line in f)
+            with open(base_file, 'r') as f:
+                pbar = tqdm(f, total=num_lines, desc="Building tuple for split: TEST")
+                for line in f:
+                    raw_p = json.loads(line)
+                    id_p = raw_p[0]
+                    edu = format_profile(raw_p[3])
+                    if id_p in right_ind.keys():
+                        ipdb.set_trace()
+                        rpf.write("ID " + str(id_p) + "===============================================================\n")
+                        rpf.write("INDUSTRY " + str(right_ind[id_p]["ind_pred"]))
+                        rpf.write("EDUCATION \n")
+                        for num, item in enumerate(edu):
+                            rpf.write("(" + num + ")" + item + "\n")
+                    elif id_p in wrong_ind.keys():
+                        ipdb.set_trace()
+                        wpf.write("ID " + str(id_p) + "===============================================================\n")
+                        wpf.write("INDUSTRY " + str(wrong_ind[id_p]["ind_lab"]))
+                        wpf.write("PREDICTION " + str(wrong_ind[id_p]["ind_pred"]))
+                        wpf.write("EDUCATION \n")
+                        for num, item in enumerate(edu):
+                            wpf.write("(" + num + ")" + item + "\n")
+                    else:
+                        continue
+                    pbar.update(1)
 
-    in_size, hidden_size, num_class_sk, num_class_ind = get_model_params(hparams, dataset_test)
-    test_loader = DataLoader(dataset_test, batch_size=1, collate_fn=collate_for_edu,
-                              num_workers=8, shuffle=True)
 
-    print("Dataloaders initiated.")
-    arguments = {'input_size': in_size,
-                 'hidden_size': hidden_size,
-                 "num_classes_skills": num_class_sk,
-                 "num_classes_ind": num_class_ind,
-                 "datadir": CFG["gpudatadir"],
-                 "hparams": hparams}
-    model = EvalModels(**arguments)
-    latest_file = get_latest_model(CFG["modeldir"], xp_title)
-    print("Evaluating model " + latest_file)
-    model.load_state_dict(torch.load(latest_file)["state_dict"])
-    model = model.cuda()
-    print("Model Loaded.")
-    #predictions = model.get_outputs(test_loader)
+def format_profile(edu_list):
+    formated_edu = []
+    sorted_edu_list = sorted(edu_list, key=lambda k: k["to"], reverse=True)
+    for num, edu in enumerate(sorted_edu_list):
+        formated_edu.append(word_seq_into_list(edu["degree"], edu["institution"], None))
+    return formated_edu
+
+def get_predictions(load):
     tgt_dir = os.path.join(CFG["gpudatadir"], "edu_preds.pkl")
-    with open(tgt_dir, 'rb') as f:
-        predictions = pkl.load(f)
-    # with open(tgt_dir, 'wb') as f:
-    #     pkl.dump(predictions, f)
-    ipdb.set_trace()
+    if load == "True":
+        with open(tgt_dir, 'rb') as f:
+            predictions = pkl.load(f)
+    else:
+        xp_title = hparams.model_type + "_" + hparams.ft_type + str(hparams.b_size) + "_" + str(hparams.lr) + '_' + str(hparams.wd)
+        datasets = load_datasets(hparams, ["TEST"])
+        dataset_test = datasets[0]
 
-    for tup in dataset_test.tuples:
-        ids = tup[0]
-        predictions[ids]["edu"] = dataset_test.tuples[ids]["edu"]
+        in_size, hidden_size, num_class_sk, num_class_ind = get_model_params(hparams, dataset_test)
+        test_loader = DataLoader(dataset_test, batch_size=1, collate_fn=collate_for_edu,
+                                  num_workers=8, shuffle=True)
+
+        print("Dataloaders initiated.")
+        arguments = {'input_size': in_size,
+                     'hidden_size': hidden_size,
+                     "num_classes_skills": num_class_sk,
+                     "num_classes_ind": num_class_ind,
+                     "datadir": CFG["gpudatadir"],
+                     "hparams": hparams}
+        model = EvalModels(**arguments)
+        latest_file = get_latest_model(CFG["modeldir"], xp_title)
+        print("Evaluating model " + latest_file)
+        model.load_state_dict(torch.load(latest_file)["state_dict"])
+        model = model.cuda()
+        print("Model Loaded.")
+        predictions = model.get_outputs(test_loader)
+        with open(tgt_dir, 'wb') as f:
+            pkl.dump(predictions, f)
+
+    return predictions
+
+
+
+def get_right_wrong_pred(predictions, handle):
+    right_preds = {}
+    wrong_preds = {}
+    for i in predictions.keys():
+        if predictions[i][handle + "_lab"] == predictions[i][handle + "_pred"]:
+            right_preds[i] = predictions[i][handle + "_lab"]
+        else:
+            wrong_preds[i] = {"lab": predictions[i][handle + "_lab"] , "pred": predictions[i][handle + "_pred"] }
+    return right_preds, wrong_preds
+
 
 
 def load_datasets(hparams, splits):
