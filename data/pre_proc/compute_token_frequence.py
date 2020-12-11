10e55a66bbf89c0c74e7bb1b609009840664ebe8
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-12-11 13:37:30 +0100

    check tkn freq

--- data/pre_proc/compute_token_frequence.py
+++ data/pre_proc/compute_token_frequence.py
@@ -20,16 +20,17 @@ def main(args):
             index = pkl.load(f)
         ds_train = TextGenerationDataset(CFG["gpudatadir"], None, None, "TRAIN", args.ft_type, None, None, 0, True)
         token_count = Counter()
-        for prof in tqdm(ds_train.tuples, desc="Parsong dataset..."):
+        for prof in tqdm(ds_train.tuples, desc="Parsing dataset..."):
             fj_ind = prof["first_job"]
             for tok in fj_ind:
                 token_count[tok] += 1
-        all_tokens = sum(token_count.values()) - token_count[index["PAD"]]
+        all_tokens = sum(token_count.values())
         frequencies = torch.ones(len(index))
-        # for tok in token_count.keys():
-        #     frequencies[tok] = all_tokens / token_count[tok]
+        for tok in token_count.keys():
+             frequencies[tok] =  token_count[tok] / all_tokens
+        ipdb.set_trace()
         # freq = 1e-10 + frequencies / all_tokens
-        frequencies[4] = 0.2
+        # frequencies[4] = 0.2
         with open(os.path.join(CFG["gpudatadir"], "token_frequencies.pkl"), "wb") as f:
             pkl.dump(frequencies, f)
 
