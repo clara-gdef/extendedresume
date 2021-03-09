ffa43c4a3932ecbcc3fbad2eef667ad721a86e69
Clara Gainon de Forsan <clara.gdef@gmail.com>
2021-03-09 12:20:42 +0100

    wip

--- data/exploration/profile_lengths_exploration.py
+++ data/exploration/profile_lengths_exploration.py
@@ -9,7 +9,7 @@ import yaml
 from collections import Counter
 import ipdb
 import fastText
-from data.datasets import FlatProfilesDataset
+from data.datasets import FlatProfilesDataset, TextGenerationDataset
 
 
 def main(args):
@@ -18,49 +18,69 @@ def main(args):
         CFG = yaml.load(ymlfile, Loader=yaml.SafeLoader)
     with ipdb.launch_ipdb_on_exception():
 
-        counter_edu = Counter()
-        all_edu = []
-        counter_jobs = Counter()
-        all_jobs = []
-        len_edu = []
-        len_jobs = []
+        # counter_edu = Counter()
+        # counter_jobs = Counter()
+        all_edu, all_jobs, len_edu, len_jobs = [], [], [], []
+        total_edu = 0; total_job = 0
     
-        for item in ["TRAIN", "VALID", "TEST"]: 
-            input_file = os.path.join(CFG["gpudatadir"], f"{args.base_file}_{item}.json")
-            with open(input_file, 'r') as f:
-                num_lines = sum(1 for line in f)
-            with open(input_file, 'r') as f:
-                pbar = tqdm(f, total=num_lines)
-                for line in f:
-                    raw_p = json.loads(line)
-                    counter_jobs[len(raw_p[1])] += 1
-                    for nj, _ in enumerate(raw_p[1]):
-                        pos_len = len(raw_p[1][nj]["position"].split(" "))
-                        desc_len = len(raw_p[1][nj]["description"].split(" "))
-                        len_jobs.append(pos_len + desc_len)
-                    all_jobs.append(len(raw_p[1]))
-                    counter_edu[len(raw_p[3])] += 1
-                    for ne, _ in enumerate(raw_p[3]):
-                        deg_len = len(raw_p[3][ne]["degree"].split(" "))
-                        inst_len = len(raw_p[3][ne]["institution"].split(" "))
-                        len_edu.append(deg_len + inst_len)
-                    all_edu.append(len(raw_p[3]))
-                    pbar.update(1)
-            print(f"NUM OCCURENCE FOR {item} SPLIT")
-            # print(f"90 percentile num edu per profile : {np.percentile(all_edu, 90)}") # 4
-            # print(f"90 percentile num jobs per profile : {np.percentile(all_jobs, 90)}") # 8
-            print(f"MEDIAN num edu per profile : {np.percentile(all_edu, 50)}") #
-            print(f"MEDIAN percentile num jobs per profile : {np.percentile(all_jobs, 50)}") #
-            print(f"MEAN num edu per profile : {np.mean(all_edu)}") # 4
-            print(f"MEAN num jobs per profile : {np.mean(all_jobs)}") # 8
-            print(f" {'='*10}")
-            print(f" SEQUENCE LENGTHS FOR {item} SPLIT")
-            print(f"Median length of edu sequence : {np.percentile(len_edu, 50)}") #
-            print(f"Median length of jobs sequence : {np.percentile(len_jobs, 50)}") #
-            print(f"MEAN length of edu sequence :{np.mean(len_edu)}") #
-            print(f"MEAN length of jobs sequence : {np.mean(len_jobs)}") #
-            print(f" {'='*30}")
-            ipdb.set_trace()
+        for item in ["TRAIN", "VALID", "TEST"]:
+            if args.load_dataset == "False":
+                input_file = os.path.join(CFG["gpudatadir"], f"{args.base_file}_{item}.json")
+                with open(input_file, 'r') as f:
+                    num_lines = sum(1 for line in f)
+                with open(input_file, 'r') as f:
+                    pbar = tqdm(f, total=num_lines, desc=f"Parsing for split {item}...")
+                    for line in f:
+                        raw_p = json.loads(line)
+                        # counter_jobs[len(raw_p[1])] += 1
+                        for nj, _ in enumerate(raw_p[1]):
+                            pos_len = len(raw_p[1][nj]["position"].split(" "))
+                            desc_len = len(raw_p[1][nj]["description"].split(" "))
+                            len_jobs.append(pos_len + desc_len)
+                        all_jobs.append(len(raw_p[1]))
+                        # counter_edu[len(raw_p[3])] += 1
+                        for ne, _ in enumerate(raw_p[3]):
+                            deg_len = len(raw_p[3][ne]["degree"].split(" "))
+                            inst_len = len(raw_p[3][ne]["institution"].split(" "))
+                            len_edu.append(deg_len + inst_len)
+                        all_edu.append(len(raw_p[3]))
+                        pbar.update(1)
+                print(f"NUM OCCURENCE FOR {item} SPLIT")
+                # print(f"90 percentile num edu per profile : {np.percentile(all_edu, 90)}") # 4
+                # print(f"90 percentile num jobs per profile : {np.percentile(all_jobs, 90)}") # 8
+                print(f"MEDIAN num edu per profile : {np.percentile(all_edu, 50)}") #
+                print(f"MEDIAN percentile num jobs per profile : {np.percentile(all_jobs, 50)}") #
+                print(f"MEAN num edu per profile : {np.mean(all_edu)}") # 4
+                print(f"MEAN num jobs per profile : {np.mean(all_jobs)}") # 8
+                print(f" {'='*10}")
+                print(f" SEQUENCE LENGTHS FOR {item} SPLIT")
+                print(f"NUM OF EDUCATION : {len(len_edu)}") #
+                print(f"NUM OF JOBS : {len(len_jobs)}") #
+                print(f"Median length of edu sequence : {np.percentile(len_edu, 50)}") #
+                print(f"Median length of jobs sequence : {np.percentile(len_jobs, 50)}") #
+                print(f"MEAN length of edu sequence :{np.mean(len_edu)}") #
+                print(f"MEAN length of jobs sequence : {np.mean(len_jobs)}") #
+                print(f" {'='*30}")
+                total_edu += len(len_edu)
+                total_job += len(len_jobs)
+                print(f"NUM OF EDUCATION : {total_edu}")  #
+                print(f"NUM OF JOBS : {total_job}")  #
+            else:
+                common_hparams = {
+                    "input_file": None,
+                    "index": None,
+                    "embedder": None,
+                    "max_seq_length": None,
+                    "datadir": CFG["gpudatadir"],
+                    "ft_type": "fs",
+                    "load": (args.load_dataset == "True"),
+                    "subsample": 0
+                }
+                edu_len = []
+                dataset = TextGenerationDataset(**common_hparams, split=item)
+                for person in dataset["tuples"]:
+                    edu_len.append(person["job_len"])
+                ipdb.set_trace()
 
 
 if __name__ == "__main__":
